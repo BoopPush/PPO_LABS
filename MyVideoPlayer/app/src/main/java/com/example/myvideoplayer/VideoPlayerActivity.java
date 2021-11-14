f27baf6fc9d3737ef4446fb7562a9ad270b1a7b2
BoopPush <ld01@tut.by>
2021-11-14 17:03:30 +0300

    setting intents

--- MyVideoPlayer/app/src/main/java/com/example/myvideoplayer/VideoPlayerActivity.java
+++ MyVideoPlayer/app/src/main/java/com/example/myvideoplayer/VideoPlayerActivity.java
@@ -3,12 +3,21 @@ package com.example.myvideoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
+    private TextView videoNameTV,videoTimeTV;
+    private ImageButton backIB,forwardIB,playpauseIB;
+    private SeekBar videoSeekBar;
+    private String videoName, videoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        videoName = getIntent().getStringExtra("videoName");
+        videoPath = getIntent().getStringExtra("videoPath");
     }
 }
